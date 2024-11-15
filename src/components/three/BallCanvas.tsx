8997a8564021b120d7e740171542e22accf1508b
Ni <daisytsai11336@gmail.com>
2024-11-15 18:29:22 +0800

    feat: ball bouncing when entering the page

--- src/components/three/BallCanvas.tsx
+++ src/components/three/BallCanvas.tsx
@@ -1,22 +1,19 @@
 "use client";
 
-import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
-// import BallModel from "./BallModel";
 
 const BallModel = dynamic(() => import("./BallModel"), { ssr: false });
 
 export default function BallCanvas() {
   return (
-    <Canvas gl={{antialias: true}} dpr={[1, 1.5]} camera={{ position: [0, 0, 5]}}>
+    <Canvas gl={{antialias: true}} dpr={[1, 1.5]} camera={{ position: [-0.5, 0, 4]}}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 10, 5]} intensity={2} />
       <Suspense fallback={null}>
         <BallModel />
       </Suspense>
-      <OrbitControls />
     </Canvas>
   );
 }
--- src/components/three/BallModel.tsx
+++ src/components/three/BallModel.tsx
@@ -1,19 +1,30 @@
 "use client";
 
 import { useGLTF } from "@react-three/drei";
-import { useEffect } from "react";
+import * as THREE from "three";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 useGLTF.preload("/models/ball.glb");
 
 export default function BallModel() {
   const BallPath = "/models/ball.glb";
   const { scene } = useGLTF(BallPath);
+  const modelRef = useRef<THREE.Object3D>();
 
-  useEffect(() => {
-    console.log(scene)
-  }, [])
+  useGSAP(() => {
+    if (!modelRef.current) {
+      return;
+    }
+    if (modelRef.current) {
+      gsap.fromTo(
+        modelRef.current.position,
+        { x: -1, y: 8, z: 0 },
+        { x: -1, y: 1, z: 0, duration: 3, ease: "bounce.out" }
+      );
+    }
+  }, [modelRef]);
 
-  return (
-      <primitive object={scene} scale={2} />
-  )
+  return <primitive object={scene} scale={50} ref={modelRef} />;
 }
